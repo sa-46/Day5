6bedd16661f97891080756f681570bc89fe89bd1
yunghans <yunghans@hotmail.com>
2018-02-10 12:08:20 +0800

    Demonstration of staged file

---
+++ Program3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program3
+    {
+
+        static void Main()
+        {
+            int x = InputUtility.ReadInteger("Please enter x: ");
+            int y = InputUtility.ReadInteger("Please enter y: ");
+            Console.WriteLine("You have entered {0} and {1}", x,y);
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program4
+    {
+        static void Main()
+        {
+            int x = InputUtility.ReadInteger();
+            Console.WriteLine(x);
+        }
+    }
+}
