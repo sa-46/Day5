1a4a5350c5da683ed74653dc56d73ca71076eaf6
yunghans <yunghans@hotmail.com>
2018-02-10 12:08:51 +0800

    Committing all changes before lunch

---
+++ InputUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class InputUtility
+    {
+        public static int ReadInteger(string prompt)
+        {
+            bool isParseDone;
+            int userInput;
+            do
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                isParseDone = Int32.TryParse(input, out userInput);
+            } while (isParseDone == false);
+
+            return userInput;
+        }
+
+        public static int ReadInteger()
+        {
+            return ReadInteger("Please enter an integer: ");
+
+            //int x = ReadInteger("Please enter an integer: ");
+            //return x;
+        }
+    }
+}
